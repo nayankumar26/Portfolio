f1083486e86bd83559943ee2df03fbb57723be27
nayankumar26 <112856399+nayankumar26@users.noreply.github.com>
2024-08-06 23:09:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const target = document.querySelector(link.getAttribute('href'));
+            target.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
+
